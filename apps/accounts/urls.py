000b82a3133f05d9c2292569dd35449dd32df6c5
Aryan <aryan.tiwary@adrosonic.com>
2026-03-03 16:09:14 +0530

    re-added refresh token logic

--- apps/accounts/urls.py
+++ apps/accounts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from .views import *
+from rest_framework_simplejwt.views import TokenRefreshView
 
 urlpatterns = [
     # Auth Flow
@@ -13,4 +14,7 @@ urlpatterns = [
     path('users/assign/<int:user_id>/', AssignRole.as_view(), name='assign_role'),
     path('users/delete/<int:user_id>/', DeleteUser.as_view(), name='delete_user'),
     path('user-rbac-audit/', AdminUserRBACListView.as_view(), name='admin-rbac-audit'),
+
+    #for refresh token 
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
